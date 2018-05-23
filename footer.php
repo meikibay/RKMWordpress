e435b3772f9e0e83cea978b7831644bf12f170b1
Miguel Ferreira <meikibay@gmail.com>
2018-05-23 22:02:07 +0200

    Baseline footer reference

---
+++ footer.php
@@ -0,0 +1,38 @@
+	<footer class="footer">
+		<section class="level">
+			<section class="content has-text-centered level-item">
+				<a href="https://www.facebook.com/">
+					<h2>
+						<span class="icon">
+		  				<i class="fa fa-facebook-official"></i>
+						</span>
+					</h2>
+				</a>
+				<a href="https://www.instagram.com/">
+					<h2>
+						<span class="icon">
+		  					<i class="fa fa-instagram"></i>
+						</span>
+					</h2>
+				</a>
+				<a href="https://www.twitter.com">
+					<h2>
+						<span class="icon">
+		  					<i class="fa fa-twitter"></i>
+						</span>
+					</h2>
+				</a>
+			</section>
+			<section class="content has-text-centered level-item">
+			</section>
+			<section class="content has-text-centered level-item">
+				<h2>
+					<span class="icon">
+	  					<i class="fa fa-phone-square"></i>
+					</span>
+				</h2>
+			</section>
+		</section>
+	</footer>
+</body>
+</html>
